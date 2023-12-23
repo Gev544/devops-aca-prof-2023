517f0372cf8f87eec50a5d098291bdf57e22c02f
Mher2019 <balayan.mher88@gmail.com>
2023-12-23 15:00:26 +0400

    -a

--- homework-19-12-2023/backend/app.py
+++ homework-19-12-2023/backend/app.py
@@ -5,4 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, this is the Flask backend!'
-
+if __name__ == "__main__":
+    # Please do not set debug=True in production
+    app.run(host="0.0.0.0", port=8000, debug=True)
