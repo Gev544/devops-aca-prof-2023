24c1336a5ce906a71c79e6e6c393abe5902df868
tadevosyannarek <ntadevosyan@sorenson.com>
2024-01-14 16:06:18 +0400

    add vpc and ec2 with internet access

--- homework-13-01-2024/narektadevosyan/terraform/vpc.tf
+++ homework-13-01-2024/narektadevosyan/terraform/vpc.tf
@@ -15,3 +15,27 @@ module "vpc" {
     Name = "ec2-vpc"
   }
 }
+
+resource "aws_security_group" "ec2_security_group" {
+  vpc_id = module.vpc.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2-security-group"
+  }
+
+}
+
