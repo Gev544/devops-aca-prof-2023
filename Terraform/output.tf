0c35d6aa09667346130e3a5f8c33e6f276c1b5f3
Arthur Manukyan <mg00artur@gmail.com>
2024-01-20 00:55:31 +0400

    Remove unneeded directories

--- Terraform/output.tf
+++
@@ -1,3 +0,0 @@
-# output "key_pair" {
-#   value = data.aws_key_pair.terraform_key_pair.key_name
-# }
--- homework-16-01-2024/arthur-manukyan/igw.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_internet_gateway" "terraform_gw" {
-  vpc_id = aws_vpc.terraform_vpc.id
-
-  tags = {
-    Name = "Terraform GW"
-  }
-}
